cdc7864199ae790e57ae18cffc6f555c2e39639c
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-23 17:16:43 +0530

    ADDED: Quadratic equation program

---
+++ FunctionalProgram/FunctionalProgram.java
@@ -0,0 +1,26 @@
+package basic_core_program.FunctionalProgram;
+
+import java.util.Scanner;
+
+public class FunctionalProgram {
+    void quadratic() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value for a :");
+        double a = sc.nextDouble();
+        System.out.print("Enter value for b :");
+        double b = sc.nextDouble();
+        System.out.print("Enter value for c :");
+        double c = sc.nextDouble();
+        double x = b * b - 4 * a * c;
+
+        if (x > 0) {
+            double root1 = (-b + Math.sqrt(x)) / (2 * a);
+            double root2 = (-b - Math.sqrt(x)) / (2 * a);
+            System.out.println("Roots are " + root1 + " " + root2);
+        } else if (x == 0) {
+            double x1 = -b / 2 * a;
+            System.out.println("The roots is :" + x1);
+        }
+    }
+
+}
