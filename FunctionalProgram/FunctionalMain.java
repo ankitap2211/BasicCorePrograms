ed1ccfe82812e09cb3f98c0559fb4de48e18684a
ankitap2211 <ankitapatil3166@gmail.com>
2022-05-23 17:25:35 +0530

    Main function

---
+++ FunctionalProgram/FunctionalMain.java
@@ -0,0 +1,10 @@
+package basic_core_program.FunctionalProgram;
+
+public class FunctionalMain {
+    public static void main(String args[]){
+        FunctionalProgram functionalprogram=new FunctionalProgram();
+        functionalprogram.distance();
+        functionalprogram.quadratic();
+        functionalprogram.sumOfThreeIntegers();
+    }
+}
